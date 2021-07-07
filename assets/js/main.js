860a61d994c02bd3145ffd5aea102900e3dd841a
Firman <fhaya.firman@gmail.com>
2021-07-07 11:15:03 +0700

    create portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,10 +93,39 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiperPortfolio = new Swiper('.portfolio__container', {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
 
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    }
+  }
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
